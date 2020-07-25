8dc62969850e555cc4b5d58c2cedd3030c813445
ppierzchalka <przemekpierzchalka@gmail.com>
2020-07-25 10:10:52 +0200

    move +1 to array generating function

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,7 @@
 export const generateArticlesData = amount =>
-  Array.from({ length: amount }, generateArticleData)
+  Array.from({ length: amount }, (_, k) => generateArticleData(k + 1))
 
-const generateArticleData = (_v, k) => {
-  const number = k + 1
+const generateArticleData = (number) => {
   return {
     id: number,
     href: '#' + number.toString(),
